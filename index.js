e982d38c011a57ee34b6f3741a148691b99d249e
HanaaChouiter <hanaa7chouiter@gmail.com>
2022-01-17 10:31:10 +0100

    ajout route modifier profil, ajouter un tweet

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express =  require("express")
 const app = express()
 const port = 5000
+const session = require("express-session")
+const cors = require("cors")
 const {dataBaseConnect} = require('./config/dataBase')
 const usersRoutes = require("./routes/users")
 const commentsRoutes = require("./routes/comments")
 const tweetsRoutes = require("./routes/tweets")
+const authRoutes = require("./routes/auth")
+const passport = require("./config/passport")
+
+app.use(session({
+    secret: 'MyAwesomeSecret', 
+    resave: true, 
+    saveUninitialized: false 
+  }))
+app.use(cors({
+    origin: "http://localhost:5000",
+    credentials: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.json())
 dataBaseConnect()
 app.use('/users', usersRoutes)
 app.use('/comments', commentsRoutes)
 app.use('/tweets', tweetsRoutes)
+app.use('/auth', authRoutes)
+
+
 
 app.listen(port,() => {
     console.log(`Server running on port ${port}`)
-} )
+})
--- routes/tweets.js
+++ routes/tweets.js
@@ -2,8 +2,47 @@ const express = require("express")
 const app = express()
 
 const Tweet = require("../models/Tweet")
+const User = require("../models/User")
 
+app.post('/:id', async (req, res)=>{
+    const { id } = req.params
 
+    try{
+        const tweet = await new Tweet({
+            ...req.body,
+            date: new Date(),
+            user: id
+        })
 
+        tweet.save(async (err, tweet) => {
+            if (tweet){
+                const getUser = await User.findById( id)
+                getUser.tweets.push(tweet._id)
+                getUser.save()
+                // console.log("getuser", getUser)
+
+                res.json(tweet)
+                return
+            }
+            console.log(err)
+            res.status(500).json({ error: err })
+        })
+    }catch (err) {
+        console.log(err)
+        res.status(500).json({ error: err })
+    }
+})
+
+app.get('/:id', async (req,res)=>{
+    const { id } = req.params
+
+    try{
+        const tweet = await Tweet.findById(id)
+
+        res.json(tweet)
+    } catch  (err){
+        res.status(500).json({error: err})
+    }
+})
 
 module.exports = app
